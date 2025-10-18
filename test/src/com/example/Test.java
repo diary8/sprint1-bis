feaec5f9531823417d124b771038497da3b59f72
diary8 <dairy.fitahiana@gmail.com>
2025-10-19 00:56:32 +0300

    Initial commit

---
+++ test/src/com/example/Test.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class Test extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>MANDE LE IZY !</h1>");
+    }
+}
